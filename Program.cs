b6a7cc7825ed0951d72cc4120b55c1aec0b78f1f
ABrunton69 <amdrummer2015@gmail.com>
2025-09-25 12:48:41 +0100

    Functional basic game

--- Program.cs
+++ Program.cs
@@ -1,10 +1,49 @@
-﻿namespace GuessTheNumberGame
+﻿using System.Reflection.PortableExecutable;
+
+namespace GuessTheNumberGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Initiate random number generator
+            Random rnd = new Random();
+
+            // Generate a random number
+            int randomNumber = rnd.Next(101);
+
+            // Users current guess
+            int userGuess = -1;
+
+            // Users guesses left
+            int guessesLeft = 20;
+            
+            // Set a variable for a correct guess
+            Boolean correctNumber = false;
+
+            do
+            {
+                
+                // Take a number from the user
+                Console.Write("Enter a number: ");
+                userGuess = Convert.ToInt32(Console.ReadLine());
+
+                // Compare the numbers
+                if (userGuess != randomNumber)
+                {
+                    if ((guessesLeft - 1) < 1)
+                    {
+                        Console.WriteLine("Incorrect! You lose!");
+                        break;
+                    }
+                    Console.WriteLine($"Incorrect {guessesLeft-1} guesses left!");
+                    guessesLeft--;
+                } else
+                {
+                    correctNumber = true;
+                    Console.WriteLine($"Congratulations! you guessed the correct number - {randomNumber}");
+                }
+            } while (correctNumber != true);
         }
     }
 }
